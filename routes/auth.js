93a29a6e8fa588dd953e97ce445b15d592735d72
jouzu1 <jouzu1@gmail.com>
2022-07-23 01:50:22 +0700

    fix register

--- routes/auth.js
+++ routes/auth.js
@@ -7,12 +7,32 @@ router.post("/register",async (req,res)=>{
     try {
         if(req.body.isAdmin){
             await newUser.save();
-            res.status(201).send({message : "New Admin is created successfully"});
+            res.status(201).send({message : "New Admin is created successfully", data : await newUser.save()});
         }
             await newUser.save();
-            res.status(201).send({message : "New User is created successfully"});
+            res.status(201).send({message : "New User is created successfully", data : await newUser.save()});
     } catch (error) {
         res.status(409).send({message : error});
+    }
+})
+
+//Login
+router.post('/login', async (req, res) => {
+    try {
+        const findUser = await User.findOne({
+            username : req.body.username
+        })
+
+        if(!findUser){
+            res.status(404).send({message : "User not found"});
+        }
+
+        if(findUser.password != req.body.password){
+            res.status(401).send({message : "Unauthorized"})
+        }else{
+        }
+    } catch (error) {
+
     }
 })
 
