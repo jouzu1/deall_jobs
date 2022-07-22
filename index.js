7d20d86ec098813a9353fe7bf512124a8be60606
jouzu1 <jouzu1@gmail.com>
2022-07-22 16:05:27 +0700

    made a connection to mongo db

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 app.listen(5000,() => {
     console.log('Server running on port', 5000);
 })
 
+mongoose
+.connect(process.env.mongo_url)
+.then(()=>console.log("Application connected successfully"))
+.catch(err => console.error(err)); 
