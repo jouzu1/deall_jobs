4ad53224cda7713f3f72da48b41898f6d4a0cd47
jouzu1 <jouzu1@gmail.com>
2022-07-24 00:44:15 +0700

    add update

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const User = require('../models/user');
+const { route } = require("./auth");
 
 const {verifyToken, verifyAdmin} = require("./verifyToken");
 
@@ -25,6 +26,17 @@ router.post("/get/user", verifyToken, async (req, res) => {
         findByUsername.map(x => user = x);
         const {password, ...getUser} = user._doc;
         res.status(200).send(getUser);
+    } catch (error) {
+        res.status(500).send({message : error});
+        console.log(error);
+    }
+})
+
+//Only Admin can access update permissions
+router.put("/update/:username", verifyAdmin, async (req, res) => {
+    try {
+        let updateUser = await User.findOneAndUpdate(req.params.username, req.body, {new:true});
+        res.status(201).send(updateUser);
     } catch (error) {
         res.status(500).send({message : error});
         console.log(error);
